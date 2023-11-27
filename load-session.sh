28c6327df1c1d5eaeee24e42b63c9a9e154fc698
Christian Krinitsin <christian.krinitsin@tum.de>
2023-11-27 16:22:33 +0100

    new load session

--- load-session.sh
+++ load-session.sh
@@ -1,2 +1,19 @@
+#
+# This script opens a session in qutebrowser. There is a problem, that if one session is already open,
+#   the new selected session is 'empty'. To avoid this problem, all qutebrowser windows are killed before
+#       opening the new session.
+#
 
-qutebrowser -r $($(cat ~/.qutesessions)/get-sessions.sh | dmenu-wl)
+sessions_path=$(realpath ~/.local/share/qutebrowser/sessions)
+
+rm ~/.local/share/qutebrowser/sessions/_autosave.yml
+
+sel=$(find $sessions_path -iname "*.yml" -exec basename {} .yml ";"  | dmenu -nb '#2b3339' -nf '#d3c6aa' -sb '#d3c6aa' -sf '#212736' -i)
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+ps -A | grep -q 'qutebrowser' && qutebrowser :wq
+
+qutebrowser -r $sel
